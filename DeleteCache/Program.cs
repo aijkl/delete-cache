4c7d80fb2a8b0988becbe9416d19a35334e3b35c
aijkl <aijkl@outlook.jp>
2021-01-18 18:17:22 +0900

    fix

--- DeleteCache/Program.cs
+++ DeleteCache/Program.cs
@@ -13,40 +13,33 @@ namespace CloudFlare
     {
         static async Task Main(string[] args)
         {
-            try
-            {
-                string json = string.Empty;
+            string json = string.Empty;
 #if DEBUG
-                json = File.ReadAllText($"{Directory.GetCurrentDirectory()}{Path.DirectorySeparatorChar}appsettings.json");
+            json = File.ReadAllText($"{Directory.GetCurrentDirectory()}{Path.DirectorySeparatorChar}appsettings.json");
 #endif
 #if !DEBUG
             json = args[0];
 #endif
-                AppSettings settings = JsonConvert.DeserializeObject<AppSettings>(json);
-                GitHubClient github = new GitHubClient(new ProductHeaderValue(settings.GitHub.UserAgent))
-                {
-                    Credentials = new Credentials(settings.GitHub.Token)
-                };
-                using CloudFlareClient cloudFlareClient = new CloudFlareClient(settings.CloudFlare.EmailAdress, settings.CloudFlare.AuthToken);
-
-                Repository repository = await github.Repository.Get(settings.GitHub.Username, settings.GitHub.Repository);
-                Branch master = await github.Repository.Branch.Get(repository.Id, settings.GitHub.Branch);
-                Branch ghPages = await github.Repository.Branch.Get(repository.Id, settings.GitHub.Branch);
-                GitHubCommit commit = await github.Repository.Commit.Get(repository.Id, master.Commit.Sha);
-                var comments = github.Repository.Comment.GetAllForCommit(repository.Id, commit.Sha).Result;
-                bool purge = commit.Files.Count > 0 && comments.Count == 0 || !comments.Last().Body.Equals("skip ci");
-                if (purge)
-                {
-                    var res = await cloudFlareClient.Zone.PurgeFilesByUrl(settings.CloudFlare.Zone, commit.Files.Select(x => $"{settings.Core.BaseUrl}/{x.Filename}").ToList());
-                    Console.WriteLine($"CloudFlare: Success:{res.Result.Success}");
-                    await github.Repository.Comment.Create(repository.Id, ghPages.Commit.Sha, new NewCommitComment("skip ci"));
-                }
-                Console.WriteLine($"PurgeFiles:{(purge ? commit.Files.Count : 0)} DateTime:{DateTime.Now} Repository:{repository.Id}");
-            }
-            catch (Exception ex)
+            AppSettings settings = JsonConvert.DeserializeObject<AppSettings>(json);
+            GitHubClient github = new GitHubClient(new ProductHeaderValue(settings.GitHub.UserAgent))
             {
-                Console.WriteLine($"Exception {ex.StackTrace}");
+                Credentials = new Credentials(settings.GitHub.Token)
+            };
+            using CloudFlareClient cloudFlareClient = new CloudFlareClient(settings.CloudFlare.EmailAdress, settings.CloudFlare.AuthToken);
+
+            Repository repository = await github.Repository.Get(settings.GitHub.Username, settings.GitHub.Repository);
+            Branch master = await github.Repository.Branch.Get(repository.Id, settings.GitHub.Branch);
+            Branch ghPages = await github.Repository.Branch.Get(repository.Id, settings.GitHub.Branch);
+            GitHubCommit commit = await github.Repository.Commit.Get(repository.Id, master.Commit.Sha);
+            var comments = github.Repository.Comment.GetAllForCommit(repository.Id, commit.Sha).Result;
+            bool purge = commit.Files.Count > 0 && comments.Count == 0 || !comments.Last().Body.Equals("skip ci");
+            if (purge)
+            {
+                var res = await cloudFlareClient.Zone.PurgeFilesByUrl(settings.CloudFlare.Zone, commit.Files.Select(x => $"{settings.Core.BaseUrl}/{x.Filename}").ToList());
+                Console.WriteLine($"CloudFlare: Success:{res.Result.Success}");
+                await github.Repository.Comment.Create(repository.Id, ghPages.Commit.Sha, new NewCommitComment("skip ci"));
             }
+            Console.WriteLine($"PurgeFiles:{(purge ? commit.Files.Count : 0)} DateTime:{DateTime.Now} Repository:{repository.Id}");            
         }
     }
 }
